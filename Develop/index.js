fd4053ed114c1a1baabe52854b43d0df7ee2032d
Samir Bello <agnide4@gmail.com>
2020-07-02 13:27:44 -0500

    Prompt quuestions set

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,87 @@
+const EditorPrompt = require("inquirer/lib/prompts/editor");
+const  fs = require('fs');
+const inquirer = require("inquirer");
+let data = "";
 // array of questions for user
+
 const questions = [
+    {
+        type: "Input",
+        name: "Title",
+        message: "What is the title of your project?"
+    },
+    { //entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+        type: "editor",
+        name: "Description",
+        message: "What does your app do?"
+    },
+    {
+        name: "Table of contents",
+        default: "Installation, Usage, License, Contributing, Tests, and Questions"
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "Software requirement",
+        default: "Basic running PC or MAC"
+    },
+    {
+        type: "Input",
+        name: "Usage",
+        message: "Give concise examples or usage cases"
+    },
+    {
+        type: "list",
+        name: "Licenses",
+        message: "What are the required licenses?",
+        choices: ['Mozilla Public License 2.0', 'GNU Lesser General Public License v3.0', "MIT License", "unlicensed"]
+
+    },
+    {
+        type: "input",
+        name: "Contributing",
+        message: "List the github user ID of key contributors"
+    },
+    {
+        type: "input",
+        name: "Tests",
+        message: "List the differents types of testing conducted"
+    },
+    {
+        type: "input",
+        name: "git hub user ID",
+        message: "What is your github user iD?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    }
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    inquirer.prompt(questions).then(function(response){
+        fs.writeFile(readme.md, utf8, response.tile){
+            
+        }
+        
+        
+    });
+    
+    
+
 }
 
 // function to initialize program
 function init() {
+    
+    
 
 }
 
+
 // function call to initialize program
 init();
+
